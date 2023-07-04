b8422ea80cef3fa5befab03cc87065be6a43ef16
Nicholas Harding <40708936+sir306@users.noreply.github.com>
2023-07-05 01:46:30 +1200

    working code
    Code seems to work but requires polishing with better layout and better code comments. A readme needs to be created and link to video tutorial

---
+++ constants.py
@@ -0,0 +1,7 @@
+LOG_FILE_PATH = 'C:\\*** ***\\GeneratedText\\log\\log.txt' # name of log file
+LOG_FOLDER_PATH = 'C:\\*** ***\\GeneratedText\\log\\' # log export path
+RDC_FILE_PATH = 'C:\\*** ***\\RenderDocFiles\\' # import path to rdc files
+GENERATED_TEXTS_FILE_PATH = 'C:\\*** ***\\GeneratedText\\' # debug script path
+LILY_IMAGE_FILE_PATH = 'C:\\*** ***\\LilyTextures\\' # export lily texture image path
+EXPORT_FBX_FILE_PATH = 'C:\\*** ***\\FBX\\'  # fbx export filepath
+
